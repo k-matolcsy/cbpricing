c1735c69ae3f51de5a5593e754c3312dca5caf5c
Kristóf <k.matolcsy92@gmail.com>
2017-11-22 22:56:53 +0100

    YieldCurve(), UsTreasury(), EuroArea() and Stock() fixes

--- bsmarket.py
+++ bsmarket.py
@@ -16,68 +16,72 @@ quandl.ApiConfig.api_key = "MGThycVq2kfvk_VR1Jn_"
 
 class YieldCurve(object):
     """
-    US zero-coupon yield curve
-    Domain range is from 0 to 30YR
+    cannot handle stress and spread at the same time
+    every time (period) is measured in years
     """
-    __stress_increase = np.array(
+    __stress_increase = np.array(   # Article 166
         [.7, .7, .7, .64, .59, .55, .52, .49, .47, .44, .42, .39, .37, .35, .34, .33, .31, .3, .29, .27, .26, .2])
-    __stress_decrease = np.array(
+    __stress_decrease = np.array(   # Article 167
         [.75, .75, .65, .56, .5, .46, .42, .39, .36, .33, .31, .3, .29, .28, .28, .27, .28, .28, .28, .29, .29, .2])
-    __stress_axis = np.array([x for x in range(21)] + [90])
+    __stress_tenors = np.array([x for x in range(21)] + [90])
 
-    def __init__(self, date, stress=None, spread=.0):
-        """
-        :param date: example "2017-09-05"
-        """
-        result_1 = spread != .0
-        result_2 = stress is not None
-        boolean = spread != .0 and stress is not None
-        if boolean:
+    def __init__(self, date, stress, spread):
+        if spread != .0 and stress is not None:
+            print("""YieldCurve() cannot handle stress and spread simultaneously""")
             raise Exception
         self.date = date
         self.spread = spread
         self.stress = stress
+        self.tenors = None  # [1 / 12, 1 / 4, 1 / 2, 1, 2, 3, 5., 7, 10, 20, 30]
+        self.rates = None  # [0.96, 1.02, 1.1, 1.24, 1.35, 1.46, 1.73, 1.99, 2.16, 2.51, 2.77]
         self.data = None
-        self.data_raw = None  # [0.96, 1.02, 1.1, 1.24, 1.35, 1.46, 1.73, 1.99, 2.16, 2.51, 2.77]
-        self.axis = None  # [1 / 12, 1 / 4, 1 / 2, 1, 2, 3, 5., 7, 10, 20, 30]
 
     def __str__(self):
         return str(self.data)
 
     @staticmethod
-    def __search_in(time, axis):
-        mn = 0
-        mx = len(axis) - 1
-        j = 0
-        if axis[mn] == time:
-            return mn
-        if axis[mx] == time:
-            return mx
-        while j < len(axis):
-            md = int((mx + mn) / 2)
-            if mn == md:
-                return None
-            if time == axis[md]:
-                return md
-            elif time < axis[md]:
-                mx = md
-                j += 1
-            else:
-                mn = md
-                j += 1
-
-    @staticmethod
-    def __search_between(time, axis):  # bisection search method between elements of an ordered list
-        if axis[0] < time < axis[-1]:
+    def __search_in(item, x):    # bisection search method between elements of an ordered list
+        if x[0] <= item <= x[-1]:
             mn = 0
-            mx = len(axis) - 1
+            mx = len(x) - 1
             j = 0
-            while j < len(axis):
+            if x[mn] == item:
+                return mn
+            if x[mx] == item:
+                return mx
+            while j <= len(x):
+                if j == len(x):
+                    print(""""search_in function failed in YieldCurve()""")
+                    raise Exception
                 md = int((mx + mn) / 2)
-                if axis[md] >= time:
+                if mn == md:
+                    return None
+                if x[md] == item:
+                    return md
+                elif x[md] > item:
                     mx = md
                     j += 1
-                elif axis[md + 1] < time:
+                else:
+                    mn = md
+                    j += 1
+        else:
+            return None
+
+    @staticmethod
+    def __search_between(item, x):   # bisection search method between elements of an ordered list
+        if x[0] < item < x[-1]:
+            mn = 0
+            mx = len(x) - 1
+            j = 0
+            while j <= len(x):
+                if j == len(x):
+                    print(""""search_between function failed in YieldCurve()""")
+                    raise Exception
+                md = int((mx + mn) / 2)
+                if x[md] >= item:
+                    mx = md
+                    j += 1
+                elif x[md + 1] < item:
                     mn = md + 1
                     j += 1
                 else:
@@ -85,112 +89,124 @@ class YieldCurve(object):
         else:
             return None
 
-    def __extrapolation(self, time, axis, data):
-        if axis[0] < time < axis[-1]:
-            k = self.__search_between(time, axis)
-            return data[k - 1] + (time - axis[k - 1]) * (data[k] - data[k - 1]) / (axis[k] - axis[k - 1])
-        elif 0 < time < axis[0]:
-            return time * data[0] / axis[0]
-        elif time <= 0:
+    def __approx(self, item, x, y):
+        if x[0] < item < x[-1]:
+            k = self.__search_between(item, x)
+            return y[k - 1] + (item - x[k - 1]) * (y[k] - y[k - 1]) / (x[k] - x[k - 1])
+        elif 0 < item < x[0]:
+            return item * y[0] / x[0]
+        elif item <= 0:
             return 0
         else:
-            return axis[-1]
+            return x[-1]
 
-    def __spot(self, time):
-        k = self.__search_in(time, self.axis)
+    def __spot(self, maturity):
+        k = self.__search_in(maturity, self.tenors)
         if isinstance(k, int):
-            return self.data_raw[k] / 100
+            return self.rates[k] / 100
         else:
-            return self.__extrapolation(time, self.axis, self.data_raw) / 100
+            return self.__approx(maturity, self.tenors, self.rates) / 100
 
-    def __spot_stress(self, time, stress):
+    def __spot_stress(self, maturity, stress):
         if stress == "inc":
-            m = 1 + self.__extrapolation(time, self.__stress_axis, self.__stress_increase)
-            return max(self.__spot(time) * m, self.__spot(time) + 0.01)
+            m = 1 + self.__approx(maturity, self.__stress_tenors, self.__stress_increase)
+            return max(self.__spot(maturity) * m, self.__spot(maturity) + 0.01)
         elif stress == "dec":
-            m = 1 - self.__extrapolation(time, self.__stress_axis, self.__stress_decrease)
-            if self.__spot(time) > 0:
-                return self.__spot(time) * m
+            m = 1 - self.__approx(maturity, self.__stress_tenors, self.__stress_decrease)
+            if self.__spot(maturity) > 0:
+                return self.__spot(maturity) * m
             else:
-                return self.__spot(time)
+                return self.__spot(maturity)
 
-    def __spot_spread(self, time, spread):
-        return self.__spot(time) + spread
+    def __spot_spread(self, maturity, spread):
+        return self.__spot(maturity) + spread
 
-    def spot(self, time):
+    def spot(self, maturity):
         if self.stress is not None:
-            return self.__spot_stress(time, self.stress)
+            return self.__spot_stress(maturity, self.stress)
         elif self.spread != 0:
-            return self.__spot_spread(time, self.spread)
+            return self.__spot_spread(maturity, self.spread)
         else:
-            return self.__spot(time)
+            return self.__spot(maturity)
 
-    def forward(self, start, end):
-        return ((1 + self.spot(end)) ** end / (1 + self.spot(start)) ** start) ** (1 / (end - start)) - 1
+    def forward(self, closer, further):
+        return ((1 + self.spot(further)) ** further / (1 + self.spot(closer)) ** closer) ** (1 / (further - closer)) - 1
 
-    def df(self, end, start=0):
-        return 1 / (1 + self.forward(start, end)) ** (end - start)
+    def df(self, maturity, base=0):
+        return 1 / (1 + self.forward(base, maturity)) ** (maturity - base)
 
-    def af(self, end, start=0, step=1):
-        def my_range(end_range, start_range, step_range):
-            if start_range < end_range and step_range > 0:
-                current = end_range
-                while current > start_range:
+    def af(self, maturity, base=0, freq=1):
+        def my_range(end, start, step):
+            if start < end and step > 0:
+                current = end
+                while current > start:
                     yield current
-                    current -= step_range
+                    current -= step
 
         s = 0
-        for x in my_range(end, start, step):
-            s += self.df(x, start)
+        for x in my_range(maturity, base, freq):
+            s += self.df(x, base)
         return s
 
     def show(self):
-        axis = np.array([0, 1 / 12, 1 / 4, 1 / 2, 3 / 4] + [x for x in range(1, 31)])
-        curve = np.array([self.__spot(x) for x in axis])
+        tenors = np.array([0, 1 / 12, 1 / 4, 1 / 2, 3 / 4] + [x for x in range(1, 31)])
+        rates = np.array([self.__spot(x) for x in tenors])
         if self.stress is not None:
-            curve_inc = np.array([self.__spot_stress(x, "inc") for x in axis])
-            curve_dec = np.array([self.__spot_stress(x, "dec") for x in axis])
-            plt.plot(axis, curve, axis, curve_inc, '--', axis, curve_dec, '--')
+            curve_inc = np.array([self.__spot_stress(x, "inc") for x in tenors])
+            curve_dec = np.array([self.__spot_stress(x, "dec") for x in tenors])
+            plt.plot(tenors, rates, tenors, curve_inc, '--', tenors, curve_dec, '--')
         elif self.spread != 0:
-            curve_spread = np.array([self.__spot_spread(x, self.spread) for x in axis])
-            plt.plot(axis, curve, '--', axis, curve_spread)
+            curve_spread = np.array([self.__spot_spread(x, self.spread) for x in tenors])
+            plt.plot(tenors, rates, '--', tenors, curve_spread)
         else:
-            plt.plot(axis, curve)
+            plt.plot(tenors, rates)
         plt.show()
         return None
 
 
 class UsTreasury(YieldCurve):
-    def __init__(self, date, stress=None):
-        super().__init__(date, stress)
+    """"
+    spread is not implemented
+    """
+    def __init__(self, date=str(dt.date.today()), stress=None):
+        super().__init__(date, stress, spread=.0)
+        # get data from Quandl
         self.data = quandl.get("USTREASURY/YIELD", start_date=date, end_date=date)  # pandas DataFrame
-        self.data_raw = self.data.values[0]
-        # time-axis
-        head = self.data.columns.values  # numpy n dim array
-        size = len(head)
-        self.axis = np.empty(size, dtype=float)  # numpy n dim array
-        for i, x in enumerate(head):
+        # get rates from data
+        self.rates = self.data.values[0]
+        # get tenors from data
+        header = self.data.columns.values  # numpy n dim array
+        self.tenors = np.empty(len(header), dtype=float)  # numpy n dim array
+        for i, x in enumerate(header):
             # everything but the last three characters
-            self.axis[i] = float(x[:-3])
+            self.tenors[i] = float(x[:-3])
             # last two characters
             if x[-2:] == "MO":
-                self.axis[i] /= 12
+                self.tenors[i] /= 12
 
 
 class EuroArea(YieldCurve):
-    def __init__(self, date, stress=None, spread=.0):
+    def __init__(self, date=str(dt.date.today()), stress=None, spread=.0):
         super().__init__(date, stress, spread)
+        # build tenors
+        self.tenors = np.array([1 / 4, 1 / 2, 3 / 4] + [x for x in range(1, 31)])
+        # get rates from ECB web
         if date == str(dt.date.today()):
-            self.data_raw = ecb.Scraper().data()
+            self.rates = ecb.Scraper().data()
+            print("""
+            Euro area yield curve from website of ECB \n
+            """)
+            for tenor, rate in zip(self.tenors, self.rates):
+                print("\t", tenor, "\t\t", rate)
         else:
-            self.data_raw = self.__parser(date)
-        self.axis = np.array([1 / 4, 1 / 2, 3 / 4] + [x for x in range(1, 31)])
-        self.data = dict(zip(self.axis, self.data_raw))
+            self.rates = self.__local_parser(date)
+        # build data
+        self.data = dict(zip(self.tenors, self.rates))
 
     @staticmethod
-    def __parser(date):
-        checklist = ['3M', '6M', '9M'] + [str(i) + 'Y' for i in range(1, 31)]
-        data = np.empty(len(checklist), dtype=float)
+    def __local_parser(date):
+        tenor_checklist = ['3M', '6M', '9M'] + [str(i) + 'Y' for i in range(1, 31)]
+        result = np.empty(len(tenor_checklist), dtype=float)
         with open("euro_area.csv", "r") as file:
             for line in file:
                 date_search = re.search(str(date), line, re.I)
@@ -198,8 +214,8 @@ class EuroArea(YieldCurve):
                     name_search = re.search('[0-9]+Y|[369]M', line, re.I)
                     if name_search:
                         rate_search = re.search('[-]?[0-9]+[.][0-9]{4}', line)
-                        data[checklist.index(name_search.group())] = rate_search.group()
-        return data
+                        result[tenor_checklist.index(name_search.group())] = rate_search.group()
+        return result
 
 
 class Stock(object):
@@ -209,36 +225,37 @@ class Stock(object):
     # symmetric_adj()
 
     def __init__(self, ticker, start=str(dt.date.today() - rel_delta(years=1)), end=str(dt.date.today()), stress=None):
-        self.ticker = ticker
-        self.start = start
-        self.end = end
-        self.stress = stress
-
+        # get data from Yahoo finance
+        table = None
         fail = True
         attempt = 1
         while fail:
             try:
-                self.table = web.DataReader(ticker, 'yahoo', start, end)
+                table = web.DataReader(ticker, 'yahoo', start, end)
                 fail = False
             except Exception as e:
-                print(str(attempt), "failed     ", str(e))
-        self.data = self.table['Adj Close']
+                print(str(attempt), "attempt failed \t", str(e))
+        self.data = table['Adj Close']
 
+        # get price from data
         if stress == "type1":
-            self.price = float(self.data.values[-1]) * (1 - self.__stress_1 - self.symmetric_adj())
+            price = float(self.data.values[-1]) * (1 - self.__stress_1 - self.symmetric_adj())
         elif stress == "type2":
-            self.price = float(self.data.values[-1]) * (1 - self.__stress_2 - self.symmetric_adj())
+            price = float(self.data.values[-1]) * (1 - self.__stress_2 - self.symmetric_adj())
         else:
-            self.price = float(self.data.values[-1])
+            price = float(self.data.values[-1])
+        self.price = price
 
     def __str__(self):
         return str(self.data)
 
     def vol_hist(self, days=252):
         n = min(days, len(self.data) - 1)
-        data = np.array([self.data.values[-x] / self.data.values[-x - 1] - 1 for x in range(1, n + 1)])
-        std_deviation = np.std(data)
-        return std_deviation * 252 ** 0.5
+        if n == len(self.data) - 1:
+            print("There is not enough data \n")
+            print("Historical volatility is calculated from" + str(n) + "observation instead of" + str(days))
+        returns = np.array([self.data.values[-x] / self.data.values[-x-1] - 1 for x in range(1, n+1)])
+        return np.std(returns) * 252 ** 0.5
 
     @staticmethod
     def symmetric_adj():
@@ -247,7 +264,7 @@ class Stock(object):
         tickers = np.array(
             ['^AEX', '^FCHI', '^GDAXI', '^FTAS', 'FTSEMIB.MI', '^IBEX', '^SSMI', '^GSPC', '^OMX', '^N225'])
         weights = np.array([.14, .14, .14, .14, .08, .08, .02, .08, .08, .02])
-
+        # get index data
         index = []
         for ticker in tickers:
             fail = True
@@ -257,19 +274,19 @@ class Stock(object):
                     index.append(web.DataReader(ticker, 'yahoo', start, end)['Adj Close'])
                     fail = False
                 except Exception as e:
-                    print(str(ticker), str(attempt), "attempt failed    ", str(e))
+                    print(str(ticker), str(attempt), "attempt failed \t", str(e))
                     attempt += 1
-
+        # calculate current index (CI) value
         ci = 0
         for x, w in zip(index, weights):
             ci += w * x[-1]
-
+        # calculate average index (AI) value
         ai = 0
         for x, w in zip(index, weights):
             ai += w * np.mean(x)
-
-        symmetric_adj_raw = 0.5 * ((ci - ai) / ai - .08)
-        return max(-.1, min(symmetric_adj_raw, .1))
+        # calculate symmetric adjustment
+        symmetric_adj = 0.5 * ((ci - ai) / ai - .08)
+        return max(-.1, min(symmetric_adj, .1))
 
 
 class Option(object):
@@ -408,5 +425,4 @@ class Bond(object):
 
 
 if __name__ == '__main__':
-    my_yield_curve = EuroArea("2017-11-22")
-    print(my_yield_curve.data_raw)
+    my_yield_curve = EuroArea()
