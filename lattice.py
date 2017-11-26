9f85c859a3cd0d80e6b12957b8cfe6bed7c60812
Kristóf <k.matolcsy92@gmail.com>
2017-11-26 21:24:38 +0100

    american cb without credit risk

--- lattice.py
+++ lattice.py
@@ -15,13 +15,13 @@ class BinomialTree(object):
 
 class CRRTree(BinomialTree):
 
-    def __init__(self, ticker, steps, option_type='call', maturity=1, strike=None):
+    def __init__(self, steps, ticker, option_type='call', maturity=1, strike=None):
         # pass input to parent
         super().__init__(steps)
 
         # save inputs
-        self.ticker = ticker
         self.steps = steps
+        self.ticker = ticker
         self.option_type = option_type
         self.maturity = maturity        # years
         self.strike = strike
@@ -39,7 +39,7 @@ class CRRTree(BinomialTree):
             historical: {}
             implied: {} 
             """.format(self.stock.vol_hist, self.option.vol_implied()))
-        self.dt = maturity / self.steps
+        self.dt = self.maturity / self.steps
         self.up = math.exp(self.volatility * self.dt ** 0.5)
         self.down = 1 / self.up
 
@@ -82,7 +82,91 @@ class CRRTree(BinomialTree):
                 self.data[j][i] = (self.data[j][i], df * (p * self.data[j+1][i][1] + (1-p) * self.data[j+1][i+1][1]))
 
 
-if __name__ == "__main__":
-    apple = CRRTree("AAPL", 60, strike=175, maturity=1)
-    print(apple.data[0])
+class ConvertibleTree(BinomialTree):
 
+    def __init__(self, principal, maturity, coupon, freq, steps, ticker, conversion_ratio):
+        # first_coupon = maturity % freq
+        # max_dt = math.gcd(first_coupon, freq)
+        # if max_dt < maturity / steps:
+        #     print("Number of steps is not enough")
+        #     raise Exception
+        # pass input to parent
+        super().__init__(steps)
+
+        # save inputs
+        self.principal = principal
+        self.maturity = maturity        # years
+        self.coupon = coupon
+        self.freq = freq
+        self.steps = steps
+        self.ticker = ticker
+        self.cr = conversion_ratio
+
+        # objects
+        expiry = str(dt.datetime.today() + dt.timedelta(days=365*maturity))[:10]
+        strike = self.principal / self.cr       # conversion price?
+        self.option = Option(ticker, 'call', expiry, strike)
+        self.stock = self.option.stock
+        self.yc = self.option.yield_curve
+
+        # set parameters
+        self.volatility = max(self.stock.vol_hist, self.option.vol_implied())
+        print("""
+            Volatility
+            historical: {}
+            implied: {} 
+            """.format(self.stock.vol_hist, self.option.vol_implied()))
+        self.dt = self.maturity / self.steps
+        self.up = math.exp(self.volatility * self.dt ** 0.5)
+        self.down = 1 / self.up
+
+        # build stock tree
+        self.__build_stock()
+
+        # build option tree
+        print(self.option.price)
+        print(self.option.maturity)
+        self.__build_derivative()
+
+    def __build_stock(self):
+        self.data[0][0] = self.stock.price
+        for i in range(1, self.size):
+            for j in range(i, self.size):
+                self.data[j][i - 1] = self.data[j - 1][i - 1] * self.up
+            self.data[i][i] = self.data[i - 1][i - 1] * self.down
+        return None
+
+    def __payoff_function(self, stock):
+        return max((1 + self.coupon) * self.principal, self.cr * stock)
+
+    def __prob(self, step):
+        f = self.yc.forward(step, step + self.dt)
+        print("forward: " + str(f))
+        return ((1 + f) ** self.dt - self.down) / (self.up - self.down)
+
+    def __coupon(self, step):
+        if step % self.freq == 0:
+            return self.coupon * self.principal
+        else:
+            return 0
+
+    def __build_derivative(self):
+        for i in range(self.size):
+            self.data[-1][i] = (self.data[-1][i], self.__payoff_function(self.data[-1][i]))
+        for j in reversed(range(self.size-1)):
+            c = self.__coupon(j*self.dt)
+            p = self.__prob(j*self.dt)
+            df = self.yc.df((j+1)*self.dt, j*self.dt)
+            print(j*self.dt, c)
+            for i in range(j+1):
+                if self.cr * self.data[j][i] > df * (p * self.data[j+1][i][1] + (1-p) * self.data[j+1][i+1][1]) + c:
+                    print("Early conversion")
+                self.data[j][i] = (self.data[j][i], max(df * (p * self.data[j+1][i][1] + (1-p) * self.data[j+1][i+1][1])
+                                                        + c, self.cr * self.data[j][i]))
+
+
+if __name__ == "__main__":
+    apple = ConvertibleTree(1000, 1, .0, .5, 4, "AAPL", 5)
+    print(apple.data[0])
+    # pear = CRRTree(4, "AAPL", "call", 1, 200)
+    # print(pear.data[0])
